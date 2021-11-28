a940d0bc4fbe089f833c8ddb65415b4ff22eba96
krzysztof-bochm <bochmkrzysztof@gmail.com>
2021-11-28 14:22:07 +0100

    added more api endpoints

--- lichess_api/src/lib.rs
+++ lichess_api/src/lib.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 use std::io::{BufRead, self};
 use std::fs;
+use chess_api::movement::{Move, Square};
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -51,8 +52,31 @@ struct ChatLine {
     text: String
 }
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Challenge {
+    id: String,
+    status: String
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Challenges {
+    #[serde(rename="in")]
+    in_challenges: Vec<Challenge>,
+    #[serde(rename="out")]
+    out_challenges: Vec<Challenge>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ChallengeRequest {
+    challenge: Challenge
+}
+
 pub struct Connection {
-    authorization: String 
+    authorization: String,
+    game_id: Option<String>
 }
 
 impl Connection {
@@ -60,7 +84,8 @@ impl Connection {
         let authorization = format!("Bearer {}", token);
 
         Connection {
-            authorization
+            authorization,
+            game_id: None
         }
     }
     
@@ -74,19 +99,25 @@ impl Connection {
         println!("{:?}", json);
     }
 
-    pub fn get_games(&self) {
+    pub fn get_games(&mut self) {
        let resp = ureq::get("https://lichess.org/api/account/playing")
            .set("Authorization", &self.authorization)
            .query("nb", "50")
            .call()
            .unwrap();
 
-        let json: Games = resp.into_json().unwrap();
+        let mut json: Games = resp.into_json().unwrap();
         println!("{:?}", json);
+        
+        if !json.now_playing.is_empty() {
+            self.game_id = Some(json.now_playing.remove(0).game_id);
+        }
     }
 
     pub fn stream_state(&self) {
-        let resp = ureq::get("https://lichess.org/api/board/game/stream/KBxUya07")
+        let game_id = self.game_id.as_ref().unwrap();
+
+        let resp = ureq::get(format!("https://lichess.org/api/board/game/stream/{}", game_id).as_str())
            .set("Authorization", &self.authorization)
            .call().unwrap();
 
@@ -125,13 +156,103 @@ impl Connection {
             }
         }
     }
+
+    pub fn stream_events() {
+        todo!();
+    }
+
+    pub fn perform_move(&self, m: Move) {
+        let game_id = self.game_id.as_ref().unwrap();
+
+        match ureq::post(format!("https://lichess.org/api/board/game/{}/move/{}", game_id, m.to_uci()).as_str())
+           .set("Authorization", &self.authorization)
+           .call() {
+            
+            Ok(_) => (),
+            Err(ureq::Error::Status(code, resp)) => panic!("{} => {}", code, resp.into_string().unwrap()),
+            Err(_) => panic!("Unknown error")
+        }
+    }
+
+    pub fn write_in_chat(&self, msg: &str) {
+        let game_id = self.game_id.as_ref().unwrap();
+        
+        ureq::post(format!("https://lichess.org/api/board/game/{}/chat", game_id).as_str())
+            .set("Authorization", &self.authorization)
+            .send_form(&[
+                ("room", "player"),
+                ("text", msg)
+            ]).unwrap();
+    }
+
+    pub fn abort_game(&self) {
+        let game_id = self.game_id.as_ref().unwrap();
+    
+        ureq::post(format!("https://lichess.org/api/board/game/{}/abort", game_id).as_str())
+            .set("Authorization", &self.authorization).call().unwrap();
+    }
+
+    pub fn resign_game(&self) {
+        let game_id = self.game_id.as_ref().unwrap();
+
+        ureq::post(format!("https://lichess.org/api/board/game/{}/resign", game_id).as_str())
+            .set("Authorization", &self.authorization).call().unwrap();
+    }
+
+    pub fn get_challenges(&self) {
+        let resp = ureq::get("https://lichess.org/api/challenge")
+            .set("Authorization", &self.authorization).call().unwrap();
+
+        let json: Challenges = resp.into_json().unwrap();
+        println!("{:?}", json);
+    }
+
+    pub fn create_challenge(&self, username: &str) {
+        let resp = ureq::post(format!("https://lichess.org/api/challenge/{}", username).as_str())
+            .set("Authorization", &self.authorization).call().unwrap();
+   
+        let json: ChallengeRequest = resp.into_json().unwrap();
+        println!("{:?}", json);
+    }
+
+    pub fn accept_challenge(&self, id: &str) {
+        ureq::post(format!("https://lichess.org/api/challenge/{}/accept", id).as_str())
+            .set("Authorization", &self.authorization).call().unwrap();
+    }
+
+    pub fn decline_challenge() {
+        todo!();
+    }
+
+    pub fn cancel_challenge() {
+        todo!();
+    }
+
+    pub fn open_challenge(&self) {
+        ureq::post("https://lichess.org/api/challenge/open")
+            .set("Authorization", &self.authorization)
+            .send_form(&[
+                ("name", "Test, DO NOT JOIN")
+            ]).unwrap();
+    }
 }
 
 
 pub fn test() {
     let token = fs::read_to_string("/home/krzysztof1222/.config/lichess_token").expect("Failed to read api token");
-    let connection = Connection::new(&token);
+    let mut connection = Connection::new(&token);
     connection.get_account();
     connection.get_games();
-    connection.stream_state();
+    //connection.stream_state();
+    //connection.perform_move(Move::new(Square::new(4, 0), Square::new(0, 0)));
+
+   // connection.write_in_chat("lol");
+   // connection.abort_game();
+   // connection.resign_game();
+
+    connection.get_challenges();
+    connection.create_challenge("Xkali");
+    // connection.accept_challenge("xd")
+    
+    connection.open_challenge();
 }
