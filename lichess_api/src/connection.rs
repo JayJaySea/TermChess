b52eddaf40ccfcdaa906d97510003c2221f07463
krzysztof-bochm <bochmkrzysztof@gmail.com>
2021-11-30 15:51:04 +0100

    made authentication look nicer

--- lichess_api/src/connection.rs
+++ lichess_api/src/connection.rs
@@ -7,9 +7,14 @@ use rand::distributions::Alphanumeric;
 
 use tiny_http::{Server, Response};
 
-use sha2::{Sha256, Sha512, Digest};
-use url::{Url, ParseError};
+use sha2::{Sha256, Digest};
+use url::Url;
 
+
+enum HttpServerError {
+    CouldNotCreateServer,
+    CouldNotReceive
+}
 
 #[derive(Deserialize, Debug)]
 struct Token {
@@ -23,6 +28,62 @@ pub struct Connection {
     token: String
 }
 
+fn create_random_string(len: usize) -> String {
+    thread_rng().sample_iter(&Alphanumeric).take(len).map(char::from).collect()
+}
+
+fn hash_string(string: &str) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.update(string);
+    hasher.finalize().to_vec()
+}
+
+fn get_verification_url(client_id: &str, code_challenge: &str, redirect_url: &str) -> String {
+    format!("https://lichess.org/oauth?response_type=code&client_id={}&redirect_uri={}&code_challenge_method=S256&code_challenge={}&scope=challenge:read%20challenge:write%20board:play",
+        client_id, redirect_url, code_challenge
+    )
+}
+
+fn get_temporary_code() -> Result<String, HttpServerError> {
+    let server = match Server::http("0.0.0.0:8080") {
+        Ok(server) => server,
+        Err(_) => return Err(HttpServerError::CouldNotCreateServer)
+    };
+
+    let code = loop {
+        let request = match server.recv() {
+            Ok(req) => req,
+            Err(_) => return Err(HttpServerError::CouldNotReceive)
+        };
+
+        let url = match Url::parse("http://localhost:8080/").unwrap().join(request.url()) {
+            Ok(url) => url,
+            Err(_) => continue
+        };
+
+        match url.query_pairs().find_map(|(name, data)| if name == "code" { Some(data) } else { None }) {
+            Some(code) => {
+                request.respond(Response::empty(200)).unwrap();
+                break String::from(code);
+            },
+            None => request.respond(Response::empty(401)).unwrap()
+        }
+    };
+
+    Ok(code)
+}
+
+fn get_access_token(code: &str, code_verifier: &str, redirect_url: &str, client_id: &str) -> Token {
+    ureq::post("https://lichess.org/api/token")
+        .send_form(&[
+            ("grant_type", "authorization_code"),
+            ("code", code),
+            ("code_verifier", code_verifier),
+            ("redirect_uri", redirect_url), 
+            ("client_id", client_id)
+        ]).unwrap().into_json().unwrap()
+}
+
 impl Connection {
     pub fn new(token: &str) -> Connection {
         Connection {
@@ -31,59 +92,18 @@ impl Connection {
     }
 
     pub fn authorize() -> Connection {
-        let code_verifier: String = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(64).map(char::from)
-            .collect();
+        let redirect_url = "http://localhost:8080/";
+        let client_id = "term-chess";
 
-        let mut hasher = Sha256::new();
-        hasher.update(&code_verifier);
-        let hashed_code_verifier = hasher.finalize();
+        let code_verifier = create_random_string(64); 
+        let code_challenge = hash_string(&code_verifier);
+        let code_challenge = base64::encode_config(code_challenge, base64::URL_SAFE_NO_PAD);
 
-        let hashed_code_verifier = base64::encode_config(hashed_code_verifier, base64::URL_SAFE_NO_PAD);
+        let verification_url = get_verification_url(client_id, &code_challenge, redirect_url);
+        println!("Open in browser: {}", verification_url);
 
-        let url = format!("https://lichess.org/oauth?response_type=code&client_id={}&redirect_uri={}&code_challenge_method=S256&code_challenge={}&scope=challenge:read%20challenge:write%20board:play",
-                "term-chess",
-                "http%3A%2F%2Flocalhost%3A8080",
-                &hashed_code_verifier
-            );
-
-        println!("code_verifier = {}", &code_verifier);
-        println!("hashed_code_verifier = {}", &hashed_code_verifier);
-        println!("{}", url);
-
-        let server = Server::http("0.0.0.0:8080").unwrap();
-
-        let token = loop {
-            let req = match server.recv() {
-                Ok(req) => req,
-                Err(e) => panic!("{:?}", e)
-            };
-
-            let req_url = req.url();
-            let this_document = Url::parse("http://localhost:8080/").unwrap();
-            let url = this_document.join(req_url).unwrap();
-            let code = url.query_pairs().find_map(|(a, b)| if a == "code" { Some(b) } else { None });
-            
-            if let Some(code) = code {
-                req.respond(Response::empty(200)).unwrap();
-                break String::from(code);
-            } else {
-                req.respond(Response::empty(401)).unwrap();
-            }
-        };
-
-        let resp = ureq::post("https://lichess.org/api/token")
-            .send_form(&[
-                ("grant_type", "authorization_code"),
-                ("code", &token),
-                ("code_verifier", &code_verifier),
-                ("redirect_uri", "http://localhost:8080"), 
-                ("client_id", "term-chess")
-            ])
-            .unwrap();
-
-        let token: Token = resp.into_json().unwrap();
+        let code = match get_temporary_code() { Ok(code) => code, Err(_) => todo!("Return http server error") };
+        let token = get_access_token(&code, &code_verifier, redirect_url, client_id);
 
         Connection::new(&token.access_token)
     }
